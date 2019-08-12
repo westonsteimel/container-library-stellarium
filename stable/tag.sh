794563b553a7824bbd55a2515d14e3b9cd6e9672
Weston Steimel <weston.steimel@gmail.com>
2019-08-12 23:08:07 +0100

    stable and master build
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- stable/tag.sh
+++ stable/tag.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=$(grep -e "ARG STELLARIUM_VERSION=" Dockerfile)
+VERSION=$(grep -e "ARG STELLARIUM_VERSION=" stable/Dockerfile)
 VERSION=${VERSION#ARG STELLARIUM_VERSION=\"}
 VERSION=${VERSION%\"}
 echo "Tagging version ${VERSION}"
