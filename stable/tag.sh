613fa81a8fdb34c658801a8037f002055eaf0571
Weston Steimel <weston.steimel@gmail.com>
2019-09-01 21:55:55 +0100

    use tag action now that docker inspect works
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- stable/tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=$(grep -e "ARG STELLARIUM_VERSION=" stable/Dockerfile)
-VERSION=${VERSION#ARG STELLARIUM_VERSION=\"}
-VERSION=${VERSION%\"}
-echo "Tagging version ${VERSION}"
-docker tag "${DOCKER_USERNAME}/stellarium:latest" "${DOCKER_USERNAME}/stellarium:${VERSION}"
